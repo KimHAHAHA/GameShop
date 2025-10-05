20c8b4b1a8efb0287a907f61395a9470a483867f
KimHAHAHA <aka.kim.285@gmail.com>
2025-10-06 00:33:59 +0700

    lo

--- gameshop/src/app/pages/admin/a-profile/a-profile.ts
+++ gameshop/src/app/pages/admin/a-profile/a-profile.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 import { HeaderAdmin } from "../../../components/header-admin/header-admin";
 import { RouterLink } from '@angular/router';
 
@@ -9,5 +9,21 @@ import { RouterLink } from '@angular/router';
   styleUrl: './a-profile.scss'
 })
 export class AProfile {
+  @ViewChild('fileInput') fileInput!: ElementRef;
+  profileImage = 'https://cdn-icons-png.flaticon.com/512/4140/4140048.png';
 
+  onImageClick() {
+    this.fileInput.nativeElement.click();
+  }
+
+  onFileSelected(event: any) {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        this.profileImage = e.target.result;
+      };
+      reader.readAsDataURL(file);
+    }
+  }
 }
--- gameshop/src/app/pages/user/login/login.ts
+++ gameshop/src/app/pages/user/login/login.ts
@@ -1,12 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [RouterLink],
+  standalone: true,
+  imports: [CommonModule, FormsModule, RouterLink],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrls: ['./login.scss']
 })
 export class Login {
+  username: string = '';
+  password: string = '';
 
+  constructor(private router: Router) {}
+
+  onLogin() {
+    if (this.username === 'user' && this.password === 'user') {
+      this.router.navigate(['/u_home']); // ไปหน้า User
+    } 
+    else if (this.username === 'admin' && this.password === 'admin') {
+      this.router.navigate(['/a_home']); // ไปหน้า Admin
+    } 
+    else {
+      alert('ชื่อผู้ใช้หรือรหัสสผ่านไม่ถูกต้อง (ลองใช้ user หรือ admin)');
+    }
+  }
 }
